a3cf95eb1d4fa748480eb780e6fcbfc1a5c1c391
delysis <georgewalkeriv@gmail.com>
2026-08-11 00:58:13 -0400

    Exercise the controlled binding feature matrix

--- llama-cpp-2/build.rs
+++ llama-cpp-2/build.rs
@@ -1,5 +1,7 @@
+//! Propagates native backend discovery from the sys crate into the safe wrapper.
+
 fn main() {
     if let Ok(dir) = std::env::var("DEP_LLAMA_BACKENDS_DIR") {
-        println!("cargo:rustc-env=GGML_BACKENDS_DIR={}", dir);
+        println!("cargo:rustc-env=GGML_BACKENDS_DIR={dir}");
     }
 }
